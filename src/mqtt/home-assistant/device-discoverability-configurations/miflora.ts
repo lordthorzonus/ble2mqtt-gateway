3a096f96560131f831ffc9a88ad5ea0dcc9ec06b
Juuso Leinonen <leinonen.juuso@gmail.com>
2024-01-14 11:53:32 +0200

    Update miflora discovery payload configuration to omit the default value

--- src/mqtt/home-assistant/device-discoverability-configurations/miflora.ts
+++ src/mqtt/home-assistant/device-discoverability-configurations/miflora.ts
@@ -57,5 +57,6 @@ export const miFloraSensorConfiguration: HomeAssistantSensorConfigurationForDevi
         device: miFloraDeviceConfiguration,
         payloadOn: true,
         payloadOff: false,
+        valueTemplate: "{{ value_json.lowBatteryWarning }}"
     },
 };
